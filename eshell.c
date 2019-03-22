aac466431fa454fa96ca4afb98cb41fb5f652f90
ewn2 <erwin.uppal@gmail.com>
2019-03-22 14:39:11 -0700

    imported tests

--- eshell.c
+++ eshell.c
@@ -5,27 +5,15 @@
 #include <math.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-void boilerplate()
-{
-   //clear();
-    printf("\n\nErwin Uppal CMPE 142 Assignment 1: Creating a UNIX Shell\n\n");
-    char* username = getenv("USER");
-    printf("\nWelcome @%s\n", username);
-    sleep(1);
-    //clear();
-}
 
-int grabber(char* str)
-{
-    char* inbuff;
-    //inbuff = readline("\n>>> ");
-}
 
 int main()
 {
-    boilerplate;
     printf("\n\nErwin Uppal CMPE 142 Assignment 1: Creating a UNIX Shell\n\n");
     char* username = getenv("USER");
     printf("\nWelcome @%s\n", username);
+    char inp[20];
+    fgets(inp,20,stdin);
+    printf("\n%c, %s\n",inp[3],inp);
     return 0;
 }
