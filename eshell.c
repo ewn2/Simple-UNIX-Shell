a7dc54b0ed1f99be0470cfdaa4ae35c05191baa4
ewn2 <erwin.uppal@gmail.com>
2019-03-22 01:11:05 -0700

    installed readline-dev

--- eshell.c
+++ eshell.c
@@ -1,11 +1,31 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+void boilerplate()
+{
+   //clear();
+    printf("\n\nErwin Uppal CMPE 142 Assignment 1: Creating a UNIX Shell\n\n");
+    char* username = getenv("USER");
+    printf("\nWelcome @%s\n", username);
+    sleep(1);
+    //clear();
+}
+
+int grabber(char* str)
+{
+    char* inbuff;
+    //inbuff = readline("\n>>> ");
+}
 
 int main()
 {
-   printf("c compiler test");
-   return 0;
+    boilerplate;
+    printf("\n\nErwin Uppal CMPE 142 Assignment 1: Creating a UNIX Shell\n\n");
+    char* username = getenv("USER");
+    printf("\nWelcome @%s\n", username);
+    return 0;
 }
